c4fe9577f53d387692b293d81ca95fd6a9838f3c
Vanya Sharma <vanya2007sharma@gmail.com>
2026-09-08 12:44:33 +0530

    Time: 7 ms (43.95%), Space: 20.4 MB (78.74%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,31 +1,61 @@
 class Solution:
-    def dfs(self, i , adj, visited, path):
-        visited[i] = 1
-        path[i] = 1
+    # def dfs(self, i , adj, visited, path):
+    #     visited[i] = 1
+    #     path[i] = 1
 
-        for neighbour in adj[i]:
-            if not visited[neighbour]:
-                if self.dfs(neighbour, adj, visited, path):
-                    return True
-            elif path[neighbour]: 
-                return True
-        path[i] = 0
-        return False
+    #     for neighbour in adj[i]:
+    #         if not visited[neighbour]:
+    #             if self.dfs(neighbour, adj, visited, path):
+    #                 return True
+    #         elif path[neighbour]: 
+    #             return True
+    #     path[i] = 0
+    #     return False
 
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        "using dfs"
-        'creating adjancency list from given input'
-        adj = [[] for _ in range(numCourses)]
-        for a, b in prerequisites:
-            adj[b].append(a)
+        # "using dfs"
+        # 'creating adjancency list from given input'
+        # adj = [[] for _ in range(numCourses)]
+        # for a, b in prerequisites:
+        #     adj[b].append(a)
 
-        'detect a cycle in graph'
-        visited = [0] * numCourses
-        path = [0] * numCourses
-        for i in range(numCourses):
-            if not visited[i]:
-                if self.dfs(i, adj, visited, path):
-                    return False
+        # 'detect a cycle in graph'
+        # visited = [0] * numCourses
+        # path = [0] * numCourses
+        # for i in range(numCourses):
+        #     if not visited[i]:
+        #         if self.dfs(i, adj, visited, path):
+        #             return False
                 
-        return True
+        # return True
+
+        #using bfs, kahns algorithm
+         # Build adjacency list
+        adj = [[] for _ in range(numCourses)]
+
+        # Indegree of each course
+        indegree = [0] * numCourses
+
+        # [course, prerequisite]
+        # prerequisite -> course
+        for course, prerequisite in prerequisites:
+            adj[prerequisite].append(course)
+            indegree[course] += 1
+        
+        q = deque() #for 0 indegree
+        for i in range(numCourses):
+            if indegree[i] == 0:
+                q.append(i)
+        count = 0
+        #kahn's algorithm 
+        while q :
+            node = q.popleft()
+            count += 1
+            for neighbour in adj[node]:
+                indegree[neighbour] -= 1
+
+                if indegree[neighbour] == 0:
+                    q.append(neighbour)
+
+        return count == numCourses
         
