fe966b76a330d58e8f34ddcb7071fe5964d78930
Vanya Sharma <vanya2007sharma@gmail.com>
2026-09-09 18:13:38 +0530

    Time: 380 ms (56.29%), Space: 0B (100%) - LeetHub

---
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -0,0 +1,16 @@
+SELECT
+    id + (
+        CASE
+            WHEN id % 2 = 1
+                 AND id != (SELECT MAX(id) FROM Seat)
+                THEN 1
+
+            WHEN id % 2 = 0
+                THEN -1
+
+            ELSE 0
+        END
+    ) AS id,
+    student
+FROM Seat
+ORDER BY id;
